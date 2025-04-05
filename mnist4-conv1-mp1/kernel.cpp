aac5dcef308c4919a2f7ab7090904f1c1dc150b1
koriel50000 <JZW00040@gmail.com>
2025-04-05 19:48:54 +0900

    Update kernel.cpp

--- mnist4-conv1-mp1/kernel.cpp
+++ mnist4-conv1-mp1/kernel.cpp
@@ -28,86 +28,11 @@ using uint6_t = ap_uint<6>;
 template <typename T>
 using win_t = hls::vector<T, KERNEL * KERNEL>;
 
-template <int W, int N>
-class int_t {
-private:
-	ap_uint<W*N> buf_;
-public:
-	int_t() : buf_(0) {}
-	int_t(int i) : buf_(i) {}
-	int_t(unsigned int ui) : buf_(ui) {}
-	int_t(long l) : buf_(l) {}
-	int_t(unsigned long ul) : buf_(ul) {}
-	int_t(const char* s) : buf_(s) {}
-
-	inline ap_range_ref<W*N, false> operator[](size_t index) const {
-		assert(index < N);
-		return buf_(W * index + W - 1, W * index);
-	}
-
-	inline ap_range_ref<W*N, false> operator[](size_t index) {
-		assert(index < N);
-		return buf_(W * index + W - 1, W * index);
-	}
-};
+template <int N>
+using int_t = hls::vector<int, N>;
 
 template <typename T>
 using fifo = hls::stream<T>;
-
-uint6_t mul66(const uint6_t i) {
-	static const uint6_t table[] = {
-		0,	0,	0,	0,	0,	0,	0,	0,
-		0,	0,	0,	1,	1,	2,	4,	8,
-		0,	0,	1,	1,	2,	4,	8,	16,
-		0,	0,	1,	2,	3,	6,	12,	24,
-		0,	1,	1,	2,	4,	8,	16,	32,
-		0,	1,	1,	3,	5,	10,	20,	40,
-		0,	1,	2,	3,	6,	12,	24,	48,
-		0,	1,	2,	4,	7,	14,	28,	56,
-	};
-	return table[i];
-}
-
-int8_t mul(const uint4_t v, const uint4_t w) {
-	uint6_t oval = mul66((v(2, 0), w(2, 0)));
-	return (v[3] ^ w[3]) == 1 ? (-oval).to_int() : oval.to_int();
-}
-
-template <int N>
-int16_t muladd(const int n, const int_t<4,N> vu, const int_t<4,N> wi) {
-	static int16_t t[N];
-#pragma HLS array_partition variable=t
-
-	for (int i = 0; i < n; i++) {
-//#pragma HLS unroll
-		t[i] = mul(vu[i], wi[i]);
-	}
-
-	for (int d = 1; d < n; d *= 2) {
-		for (int i = 0; i < n; i += d * 2) {
-//#pragma HLS unroll
-			t[i] += t[i + d];
-		}
-	}
-	return t[0];
-}
-
-uint4_t batch_norm(const int16_t acc, const int thr[], bool relu) {
-	static const uint4_t indexTable[] = {
-		0, 1, 2, 4, 7, 3, 6, 5,
-	};
-#pragma HLS array_partition variable=indexTable
-	
-	ap_uint<1> b0 = acc >= thr[0];
-	ap_uint<1> b1 = acc >= thr[1];
-	ap_uint<1> b2 = acc >= thr[2];
-	ap_uint<1> b3 = acc >= thr[3];
-	ap_uint<1> b4 = acc >= thr[4];
-	ap_uint<1> b5 = acc >= thr[5];
-	ap_uint<1> b6 = acc >= thr[6];
-	ap_uint<8> bits = (0, b6, b5, b4, b3, b2, b1, b0);
-	return indexTable[((bits + 1) * 0x17)(7, 5)];
-}
 
 template <int ROWS, int COLS, typename T, typename WT>
 class Window {
@@ -235,9 +160,17 @@ private:
 			for (int z = 0; z < f; z++) {
 				int16_t acc = 0;
 				for (int k = 0; k < KN * KN; k++) {
-					acc += muladd<C>(c, val[k], wi[z * KN * KN + k]);
+					for (int i = 0; i < 16; i++) {
+						acc += val[k][i] * wi[z * KN * KN + k][i];
+					}
 				}
-				oval[z] = batch_norm(acc, thr, true);
+				int m = 0;
+				for (int n = 0; n < 3; n++) {
+					if (acc >= thr[n]) {
+						m = n + 1;
+					}
+				}
+				oval[z] = m;
 			}
 			outb[xy] = oval;
 		}
@@ -252,7 +185,7 @@ public:
 		for (int j = 0; j < f * kn * kn; j++) {
 			T val;
 			for (int i = 0; i < c; i++) {
-				val[i] = (weight[ptr++] << 2) & 0xf;
+				val[i] = weight[ptr++];
 			}
 			wi[j] = val;
 		}
@@ -273,64 +206,12 @@ public:
 	}
 };
 
-template <typename T, int H, int W, int C>
-class MaxPool2x2 {
-private:
-	void maxpool(const T v1, const T v2, T& ov) {
-		for (int z = 0; z < C; z++) {
-#pragma HLS unroll
-			ov[z] = (v1[z] > v2[z]) ? v1[z] : v2[z];
-		}
-	}
-
-	void compute_h(const int h, const int w, const T inb[], fifo<T>& pips) {
-		int ptr = 0;
-		for (int i = 0; i < h * w / 2; i++) {
-#pragma HLS pipeline
-			T val1 = inb[ptr++];
-			T val2 = inb[ptr++];
-			T oval;
-			maxpool(val1, val2, oval);
-			pips.write(oval);
-		}
-	}
-
-	void compute_v(const int oh, const int ow, T outb[], fifo<T>& pips) {
-		static T buf[W / 2];
-#pragma HLS array_partition variable=buf
-
-		int ptr = 0;
-		for (int y = 0; y < oh; y++) {
-#pragma HLS pipeline
-			for (int x = 0; x < ow; x++) {
-				buf[x] = pips.read();
-			}
-			for (int x = 0; x < ow; x++) {
-				T val1 = buf[x];
-				T val2 = pips.read();
-				T oval;
-				maxpool(val1, val2, oval);
-				outb[ptr++] = oval;
-			}
-		}
-	}
-
-public:
-	void compute(const int h, const int w, const T inb[], T outb[]) {
-		fifo<T> pips("pipe_fifo");
-
-#pragma HLS dataflow
-		compute_h(h, w, inb, pips);
-		compute_v(h / 2, w / 2, outb, pips);
-	}
-};
-
 template <int H, int W, int C>
-void read_input(const int in[H * W * C], int_t<4,16> inb[H * W]) {
+void read_input(const int in[H * W * C], int_t<16> inb[H * W]) {
 
 	for (int xy = 0; xy < H * W; xy++) {
 #pragma HLS unroll factor=W skip_exit_check
-		int_t<4,16> val;
+		int_t<16> val;
 		for (int z = 0; z < C; z++) {
 			val[z] = in[xy];
 		}
@@ -339,11 +220,11 @@ void read_input(const int in[H * W * C], int_t<4,16> inb[H * W]) {
 }
 
 template <int H, int W, int C>
-void write_result(int out[H * W * C], const int_t<4,16> outb[H * W]) {
+void write_result(int out[H * W * C], const int_t<16> outb[H * W]) {
 	int ptr = 0;
 	for (int xy = 0; xy < H * W; xy++) {
 #pragma HLS pipeline
-		int_t<4,16> val = outb[xy];
+		int_t<16> val = outb[xy];
 		for (int z = 0; z < C; z++) {
 #pragma HLS unroll
 			out[ptr++] = val[z];
@@ -362,17 +243,17 @@ void kernel(
 #pragma HLS array_partition variable=in cyclic factor=CHANNEL
 #pragma HLS array_partition variable=out cyclic factor=FILTER
 
-	static int_t<4,16> even_buf[HEIGHT * WIDTH];
-	static int_t<4,16> odd_buf[OHEIGHT * OWIDTH];
+	static int_t<16> even_buf[HEIGHT * WIDTH];
+	static int_t<16> odd_buf[OHEIGHT * OWIDTH];
 #pragma HLS array_partition variable=even_buf cyclic factor=WIDTH
 #pragma HLS array_partition variable=odd_buf cyclic factor=OWIDTH
 
-	static int_t<4,16> conv_wi[FILTER * KERNEL * KERNEL];
-	static int conv_thr[7] = { 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff };
+	static int_t<16> conv_wi[FILTER * KERNEL * KERNEL];
+	static int conv_thr[THRESHOLD];
 #pragma HLS array_partition variable=conv_wi cyclic factor=KERNEL*KERNEL
 #pragma HLS array_partition variable=conv_thr
 
-	Conv2D<12,12,16,5,int_t<4,16>,win_t<int_t<4,16>>> conv;
+	Conv2D<12,12,16,5,int_t<16>,win_t<int_t<16>>> conv;
 
 	read_input<12,12,16>(in, even_buf);
 	conv.read(16, 5, 16, weight, threshold, conv_wi, conv_thr);
