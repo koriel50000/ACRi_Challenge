24a39f2a5d6a6c9db4ae7ea73fde032970d829e4
koriel50000 <JZW00040@gmail.com>
2025-09-28 11:02:01 +0900

    update tb.cpp

--- yunet/tb.cpp
+++ yunet/tb.cpp
@@ -3,10 +3,29 @@
 #include "params.hpp"
 
 void input_stream(hls::stream<uint64_t>& ins) {
+    // torch.Size([1, 3, 640, 640])
     for (int i = 0; i < 640 * 640; i++) {
         ins.write(images[i]);
     }
-}
+
+    // torch.Size([16, 3, 3, 3])
+    for (int i = 0; i < 16 * 9; i++) {
+        ins.write(backbone_model0_conv1_weight[i]);
+    }
+    // torch.Size([16, 7])
+    for (int i = 0; i < 16 * 7; i++) {
+        ins.write(backbone_model0_relu1_threshold[i]);
+    }
+
+    // torch.Size([16, 1, 1, 16])
+    for (int i = 0; i < 16 * 1; i++) {
+        ins.write(backbone_model0_conv2_conv1_weight[i]);
+    }
+    // torch.Size([16, 14])
+    for (int i = 0; i < 16 * 14; i++) {
+        ins.write(backbone_model0_conv2_quant1_threshold[i]);
+    }
+};
 
 int main(int argc, char** argv)
 {
