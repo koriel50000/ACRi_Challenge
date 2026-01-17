b93fffa949b2fb502d957ef2b118e5faf4abe28f
koriel50000 <JZW00040@gmail.com>
2026-01-17 16:31:48 +0900

    Update kernel.cpp

--- mnist6-free/kernel.cpp
+++ mnist6-free/kernel.cpp
@@ -238,7 +238,7 @@ for (int i = 0; i < (W + (KN - 1)) * (H + (KN - 1)); i++) {
     win oval = linebuf.get_window();
     for (int ky = 0; ky < KN; ky++) {
       for (int kx = 0; kx < KN; kx++) {
-        printf("[%d,%d]=%d ", x - (KN - 1) + kx, y - (KN - 1) + ky, oval[y * KN + i]);
+        printf("[%d,%d]=%d ", x - (KN - 1) + kx, y - (KN - 1) + ky, oval[ky * KN + kx]);
       }
       printf("\n");
 	}
